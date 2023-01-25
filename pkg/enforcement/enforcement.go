daa26b8d3aaf6f96273a34cfc64c12c35b473604
Razco <razchn@gmail.com>
2023-01-25 12:53:38 +0200

    remove client from enforcement.go

--- pkg/enforcement/enforcement.go
+++ pkg/enforcement/enforcement.go
@@ -1,13 +1,11 @@
 package enforcement
 
 import (
-	"github.com/permitio/permit-golang/openapi"
 	"github.com/permitio/permit-golang/pkg/config"
 	"go.uber.org/zap"
 )
 
 type PermitBaseEnforcer struct {
-	client *openapi.APIClient
 	config *config.PermitConfig
 	logger *zap.Logger
 }
@@ -17,10 +15,8 @@ type PermitEnforcer struct {
 }
 
 func NewPermitEnforcerClient(config *config.PermitConfig) *PermitEnforcer {
-	client := openapi.NewAPIClient(openapi.NewConfiguration())
 	return &PermitEnforcer{
 		PermitBaseEnforcer{
-			client: client,
 			config: config,
 			logger: config.Logger,
 		},
